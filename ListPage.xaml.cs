a7ae53620112ae216855835f6b3695c01b0684cf
andreikish <andreicezar.kish@gmail.com>
2024-12-05 17:02:18 +0200

    20

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -3,10 +3,11 @@ namespace KishAndreiCezarLab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -14,10 +15,51 @@ public partial class ListPage : ContentPage
         await App.Database.SaveShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
     async void OnDeleteButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+    private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            deleteItemButton.IsVisible = true;
+        }
+        else
+        {
+            deleteItemButton.IsVisible = false;
+        }
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        var selectedProduct = listView.SelectedItem as ListProduct;
+        if (selectedProduct != null)
+        {
+            await App.Database.DeleteListProductAsync(selectedProduct);
+            listView.ItemsSource = await App.Database.GetListProductsAsync(((ShopList)BindingContext).ID);
+            deleteItemButton.IsVisible = false;
+        }
     }
 }
